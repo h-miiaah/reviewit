ff27e9ad83e21c82073c66373cb0dc9779b4fe36
h-miiaah <haim.miiaah5@gmail.com>
2020-04-12 13:33:53 -0400

    associations added to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
-  belongs_to :user
+  belongs_to :user #creator of the product
+  has_many :reviews
+  has_many :users, through: :reviews #people who reviewed the product
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :reviews
+    has_many :reviewed_products, through: :reviews, source: :product
+    has_many :products #products that they created
+
+    has_secure_password
 end
