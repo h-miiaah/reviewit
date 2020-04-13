3ccf4742bc622e536d07fa3983251df17954797b
h-miiaah <haim.miiaah5@gmail.com>
2020-04-13 13:23:30 -0400

    create and view products added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,33 @@
 class ProductsController < ApplicationController
+
+    def index
+        @products = Product.all
+    end
+
+    def new
+        @product = Product.new
+        @product.build_category #build product a category using the nested attribbutes of category
+    end
+
+    def create
+        @product = Product.new(product_params)
+        @product.user_id = session[:user_id]
+        if @product.save!
+            redirect_to product_path(@product)
+        else
+            @product.build_category
+            render :new
+        end
+    end
+
+    def show
+        @product = Product.find_by_id(params[:id])
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:name,:brand, :description, :category_id, category_attributes: [:name])
+    end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,10 @@ class Product < ApplicationRecord
   belongs_to :user #creator of the product
   has_many :reviews
   has_many :users, through: :reviews #people who reviewed the product
+
+  def category_attributes=(attributes)
+    self.category = Category.find_or_create_by(attributes) if !attributes[:name].empty?
+    self.category
+  end
+
 end
