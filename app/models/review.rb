5ff88d5e9ba0e8a379ccdd2cdc4447793b62907b
h-miiaah <haim.miiaah5@gmail.com>
2020-04-13 22:25:15 -0400

    can now choose product to create a review for from the create review page

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,7 @@ class Review < ApplicationRecord
 
   validates :stars, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
   validates :title, :content, presence: true
+
+  #if user already reviewed a product and tries to review it again, it will say the user already reviewed that product
+  validates :product, uniqueness: {scope: :user, message: "was already reviewed by you"}
 end
