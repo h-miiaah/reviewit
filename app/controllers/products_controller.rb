a3ecd7953689436f98e2db101f4622fc950e4020
h-miiaah <haim.miiaah5@gmail.com>
2020-04-15 15:34:57 -0400

    added set_product instance method to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
 
+    before_action :set_product, only: [:show, :edit, :update, :delete]
+
     def index
         @products = Product.order_by_rating
     end
@@ -21,18 +23,16 @@ class ProductsController < ApplicationController
     end
 
     def show
-        @product = Product.find_by_id(params[:id])
+        
     end
 
     def edit
-        @product = Product.find_by_id(params[:id])
         if @product.user_id != current_user.id
             redirect_to products_path           
         end
     end
 
     def update
-        @product = Product.find_by_id(params[:id])
         if @product.user_id != current_user.id
             redirect_to products_path           
         end
@@ -45,7 +45,6 @@ class ProductsController < ApplicationController
 
     def destroy
         # byebug
-        @product = Product.find_by_id(params[:id])
         @product.destroy
         redirect_to products_path
     end
@@ -54,6 +53,10 @@ class ProductsController < ApplicationController
 
     def product_params
         params.require(:product).permit(:name,:brand, :description, :category_id, category_attributes: [:name])
+    end
+
+    def set_product
+        @product = Product.find_by_id(params[:id])
     end
 
 end
