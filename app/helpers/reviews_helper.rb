9edda42e3bcbf33b668d7d5a0312af224539feb3
h-miiaah <haim.miiaah5@gmail.com>
2020-04-15 18:08:04 -0400

    added more styling to views and forms

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,9 +1,9 @@
 module ReviewsHelper
     def show_header(review)
         if params[:product_id]
-            content_tag(:h1, "Write a Review for #{review.product.name}")
+            content_tag(:h1, "Write a Review for #{review.product.name}", class: "text-center")
         else
-            content_tag(:h1, "Write a Review")
+            content_tag(:h1, "Write a Review", class: "text-center")
         end
     end
 end
